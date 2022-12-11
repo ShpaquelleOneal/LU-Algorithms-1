df68eef1cd485d9fc4264016ba61f7470eac12ed
ShpaquelleOneal <109593992+ShpaquelleOneal@users.noreply.github.com>
2022-12-11 19:01:37 +0200

    task 2 done

--- Test 3 tasks/Task 2 new.cpp
+++ Test 3 tasks/Task 2 new.cpp
@@ -6,7 +6,35 @@ using namespace std;
 
 
 int main() {
+    int inp =0, n=0, flag=0;
+    int a[20];
 
+    for (int i = 0; i < 20; i++) {
+        cout << "Enter " << i+1 << ". number ";
+        cin >> inp;
+        cout << "Line: ";
+        if (i == 0) a[0] = inp;
+        else {
+            for(int j = 0; j < i; j++) {
+                if (inp > a[j]) {
+                    for (int h = 19; h>j; h--) {
+                        a[h] = a[h-1];
+                    }
+                    a[j] = inp;
+                    flag = 1;
+                    break;
+                }
+            }
+            if (!flag) a[i] = inp;
+            flag = 0;
+        }
+
+        for (int k = 0; k < i+1; k++) {
+            cout << a[k] << " ";
+        }
+        cout << endl;
+
+    }
 
 
 
